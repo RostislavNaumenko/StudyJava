45403b4646cf15e73ba7749f63a5349a3776588c
RostislavNaumenko <rostislavnaumenko.work@gmail.com>
2024-06-20 23:31:23 +0200

    unit tests for class person

--- src/lesson_27/PersonTest.java
+++ src/lesson_27/PersonTest.java
@@ -3,6 +3,11 @@ package lesson_27;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
 
 public class PersonTest {
     Person person;
@@ -26,5 +31,66 @@ public class PersonTest {
         System.out.println("getEmail After: " + person.getEmail());
     }
 
+    @ParameterizedTest
+    @MethodSource("emailNotValidTest")
+    void notValidEmail (String email){
+
+        person.setEmail(email);
+
+        Assertions.assertEquals(startEmail, person.getEmail());
+    }
+
+    static Stream<String> emailNotValidTest () {
+
+        return Stream.of(
+                        // only 1 @
+                "email@@mail.com",
+                //dot
+                "email@mailcom",
+                //after last dot > 2 symbols
+                "email@mailco.m",
+                //english alphabet, digital, '_', '-', '.', '@'
+                "++#email@mail.com",
+                // before @ min 1 symbols
+                "@mail.com");
+
+    }
+
+    @Test
+    void testValidPassword(){
+        String validPassword = "Rostyslav@1";
+
+        person.setPassword(validPassword);
+
+        Assertions.assertEquals(validPassword, person.getPassword());
+
+        System.out.println("getPassword After: " + person.getPassword());
+    }
+
+
+    @ParameterizedTest
+    @MethodSource("passwordNotValidTest")
+    void notValidPassword (String password){
+
+        person.setPassword(password);
+
+        Assertions.assertEquals(startPassword, person.getPassword());
+    }
+
+    static Stream<String> passwordNotValidTest () {
+
+        return Stream.of(
+                // length >= 8
+                "Ros@1",
+                // min 1 digital
+                "Rostyslav@",
+                // min 1 lower case
+                "ROSTYSLAV@1",
+                // min 1 upper case
+                "rostyslav@1",
+                //min 1 special symbol
+                "Rostyslav1");
+
+    }
 
 }
